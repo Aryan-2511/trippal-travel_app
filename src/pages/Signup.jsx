4ed9dce425705df281eda462a90a81d51b7b53dd
AmanSingh5689 <2021is_amansingh_a@nie.ac.in>
2024-06-13 19:30:36 +0530

    test

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -21,7 +21,7 @@ function Signup() {
       <div className="signup-main flex-column-center">
         <div className="signup-header flex-column-center">
           <h2 className="signup-header-heading">
-            Sigt up and Let Your <span>Adventures</span> Begin!
+            Sign up and Let Your <span>Adventures</span> Begin!
           </h2>
           {isFormOpen ? (
             <SignupForm isFormOpen={isFormOpen} handleForm={setIsFormOpen} />
