cb5b4f2a6d22c9419461834f69c760611e4973d6
Aryan Chaudhary <artistaryan25@gmail.com>
2024-06-22 22:18:28 +0530

    backend and folder structure

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+const app = require('./src/app');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
