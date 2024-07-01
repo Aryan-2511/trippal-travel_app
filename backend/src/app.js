6af0280ad3af1509be9c7027bee94b22a9e2d138
Aryan Chaudhary <artistaryan25@gmail.com>
2024-07-01 23:37:00 +0530

    chutiya banaya bada maza aaya

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./controllers/authController');
 const cors = require('cors');
+const places = require('./controllers/placeController');
 
 
 const app = express();
@@ -11,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth',authRoutes);
-
+app.use('/api/place',places);
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
